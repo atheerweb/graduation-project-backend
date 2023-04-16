9d0a29c98a915bd0f2104bc4bebb1c19f4b6451b
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2023-04-16 02:38:56 +0200

    added users app

---
+++ users/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class User (models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    birth_date = models.DateField(max_length=9)
+    country = models.CharField(max_length=45)
+    city = models.CharField(max_length=45)
+    Street = models.CharField(max_length=45)
+    email_address = models.EmailField(max_length=40)
