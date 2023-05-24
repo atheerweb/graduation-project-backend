d6a26759deb7f146627fe5da782622471b823388
Ziad7asn <ziad.ehab15@yahoo.com>
2023-05-24 10:53:56 +0300

    first name and user name

--- graduation/serializers.py
+++ graduation/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from users.models import User, UserRoles, Role 
-from courses.models import Category,Course
+from users.models import User, UserRoles, Role
+from courses.models import Category, Course
 from freelance.models import Job
 from rest_framework import serializers
 from accounts.models import MyUser
@@ -10,29 +10,38 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 User = get_user_model()
 # User Serializer
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
-        fields = ('id', 'username', 'email','gender','birth_date','country','city','Address')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email',
+                  'gender', 'birth_date', 'country', 'city', 'Address')
 
 # Register Serializer
+
+
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyUser
-        fields = ('id', 'username', 'email', 'password','gender','birth_date','country','city','Address')
+        fields = ('id', 'first_name', 'last_name', 'username', 'email',
+                  'password', 'gender', 'birth_date', 'country', 'city', 'Address')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = MyUser.objects.create(username = validated_data['username'], 
-        email = validated_data['email'], 
-        password = validated_data['password'],
-        gender = validated_data['gender'], 
-        birth_date = validated_data['birth_date'],
-         country = validated_data['country'],
-        city =  validated_data['city'],
-        Address = validated_data['Address'],)
+        user = MyUser.objects.create(username=validated_data['username'],
+                                     first_name=validated_data['first_name'],
+                                     last_name=validated_data['last_name'],
+                                     email=validated_data['email'],
+                                     password=validated_data['password'],
+                                     gender=validated_data['gender'],
+                                     birth_date=validated_data['birth_date'],
+                                     country=validated_data['country'],
+                                     city=validated_data['city'],
+                                     Address=validated_data['Address'],)
 
         return user
+
 
 class Userserializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -54,17 +63,18 @@ class UserRolesSerializers(serializers.ModelSerializer):
 
 
 class Get_Category(serializers.ModelSerializer):
-          class Meta:
-                model = Category
-                fields =['category_name']
+    class Meta:
+        model = Category
+        fields = ['category_name']
+
 
 class Get_Top5_Courses(serializers.ModelSerializer):
-          class Meta:
-                model = Course
-                fields =['course_name', 'price', 'duration']
+    class Meta:
+        model = Course
+        fields = ['course_name', 'price', 'duration']
 
 
 class Get_Jops(serializers.ModelSerializer):
-          class Meta:
-                model = Job
-                fields =['jop_title','descriotion']
+    class Meta:
+        model = Job
+        fields = ['jop_title', 'descriotion']
