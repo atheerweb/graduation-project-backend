39dc1d185b1d16b6f658a7d182752ef13eb6d781
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2023-06-08 02:01:38 +0300

    vercel

--- graduation/settings.py
+++ graduation/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static" 
 
 # Default primary key field type
