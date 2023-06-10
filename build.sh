6544b5457cd46b3bce47645a9bfbcb2e36d0d4db
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2023-06-10 11:03:21 +0200

    test buils.sh creating superuser

--- build.sh
+++ build.sh
@@ -12,3 +12,8 @@ python3.9 manage.py migrate --noinput
 
 echo "Collect Static......"
 python3.9 manage.py collectstatic --noinput --clear
+
+
+echo "Creating superuser......"
+from django.contrib.auth.models import User;
+User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
